8edf310afb304739c5af5dd8230b4359fba7f8d1
kotaro-miura <mcotaro070835@gmail.com>
2024-03-21 11:35:29 +0900

    TAS-92：テスト制作

--- app/cruds/mentors.py
+++ app/cruds/mentors.py
@@ -1,39 +1,34 @@
 from sqlalchemy.orm import Session
+from schemas.mentors import ResponseBody
 from models.user_account_info import UserAccountInfo
-from models.users import Users
 from models.user_account_types import UserAccountTypes
-from models.mentorships import Mentorships
-from logging import getLogger
-from schemas.mentors import ResponseBodyGet
+from models.users import Users
+from fastapi import HTTPException
 
-logger = getLogger("uvicorn.app")
+def create(db: Session, create_model: ResponseBody,mentor_id):
 
-def find_bank_info(db: Session, mentor_id: int):
+    mentor = db.query(Users).filter(Users.id == mentor_id).first()
 
-    mentor_info = db.query(Mentorships).filter(Mentorships.mentor_id == mentor_id).first()
-
-    if not mentor_info:
+    if not mentor:
         return None
+
+
+    account_type_id = db.query(UserAccountTypes).filter(create_model.account_type == UserAccountTypes.name).first()
+
+
+    new_transfer = UserAccountInfo(
+        user_id = mentor_id,
+        bank_name = create_model.bank_name,
+        branch_name = create_model.branch_name,
+        bank_code = create_model.bank_code,
+        branch_code = create_model.branch_code,
+        account_type_id = account_type_id.id,
+        account_number = create_model.account_number,
+        account_name = create_model.account_name
     
-    bank_info = db.query(UserAccountInfo).filter(UserAccountInfo.user_id == mentor_info.mentor_id).first()
-    if not bank_info:
-        return None
+        )
     
-
-    account_type = db.query(UserAccountTypes).filter(bank_info.user_id == UserAccountTypes.id).first()
-    if not account_type:
-        return None
+    db.add(new_transfer)
 
 
-    #請求履歴詳細
-    info = {
-        "mentor_id":  mentor_id,
-        "account_name": bank_info.account_name,
-        "bank_name": bank_info.bank_name,
-        "branch_name": bank_info.branch_name,
-        "account_number": bank_info.account_number,
-        "account_type": account_type.name
-       
-    }
-
-    return info
+    return new_transfer
--- app/tests/test_mentors.py
+++ app/tests/test_mentors.py
@@ -1,27 +1,51 @@
 from fastapi.testclient import TestClient
 
-"""取得成功パターン"""
+"""送金先作成"""
+"""成功パターン"""
 def test_mentors_01(client_fixture: TestClient):
-    
-    response = client_fixture.get(
-        "/mentors/1/accounts"
+
+    response = client_fixture.post(
+       "/mentors/5/accounts",
+       json={
+            "bank_name": "Aバンク",
+            "branch_name": "A支店",
+            "bank_code": "123" ,
+            "branch_code": "456",
+            "account_type": "普通", 
+            "account_number": "11-22-33",
+            "account_name": "A太郎",
+        }  
+
+
+
     )
 
-    assert response.status_code == 200
+    assert response.status_code == 201
+    assert "account_id" in response.json()
     assert "mentor_id" in response.json()
-    assert "account_name" in response.json()
     assert "bank_name" in response.json()
     assert "branch_name" in response.json()
-    assert "account_number" in response.json()
+    assert "bank_code" in response.json()
+    assert "branch_code" in response.json()
     assert "account_type" in response.json()
+    assert "account_number" in response.json()
+    assert "account_name" in response.json()
 
-"""取得失敗パターン"""
+
+"""失敗パターン"""
 def test_mentors_02(client_fixture: TestClient):
 
-    response = client_fixture.get(
-       "/mentors/111/accounts"
+    response = client_fixture.post(
+       "/mentors/112/accounts",
+        json={
+            "bank_name": "Aバンク",
+            "branch_name": "A支店",
+            "bank_code": "123" ,
+            "branch_code": "456",
+            "account_type": "普通", 
+            "account_number": "11-22-33",
+            "account_name": "A太郎",
+        }  
     )
 
     assert response.status_code == 404
-
-
