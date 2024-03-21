e27aa9546441b5c70bfcce73ec31694106b49e4c
kotaro-miura <mcotaro070835@gmail.com>
2024-03-21 13:51:52 +0900

    TAS-9：テスト仮作成

---
+++ app/tests/test_login.py
@@ -0,0 +1,44 @@
+from fastapi.testclient import TestClient
+
+"""取得成功パターン"""
+def test_login_01(client_fixture: TestClient):
+    """受け取ったpasswordとemailが元の情報と一致"""
+    response = client_fixture.post(
+        "/login",
+        json={
+            "email": "aaa@gmail.com",
+            "password": "test1234",
+            "device_info": {
+                "device_type": "smartphone",
+                "device_name": "iPhone12"
+            }
+        }   
+    )
+
+
+    assert response.status_code == 200
+    assert "user_id" in response.json()
+    assert "access_token" in response.json()
+    assert "expires_in" in response.json()
+    assert "role" in response.json()
+
+
+"""取得失敗パターン"""
+def test_login_02(client_fixture: TestClient):
+    """受け取ったpasswordとemailが元の情報と不一致"""
+    response = client_fixture.post(
+        "/login",
+        json={
+            "email": "zzz@gmail.com",
+            "password": "zzzpassword",
+            "device_info": {
+                "device_type": "smartphone",
+                "device_name": "iPhone12"
+            }
+        }   
+    )
+
+
+    assert response.status_code == 401
+    assert response.json() == {"detail":"Invalid email or password."}
+
