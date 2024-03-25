82d098b3116139d0c7f231421ed15f30bb1460cc
Inoue-Sub <shoki.inoue.1225.sub@gmail.com>
2024-03-25 16:23:55 +0900

    TAS-192（会社情報作成API）：実装完了

--- app/apis/companies.py
+++ app/apis/companies.py
@@ -6,6 +6,9 @@ from fastapi import APIRouter, Depends, HTTPException, Path, Query
 from sqlalchemy.orm import Session
 from starlette import status
 
+from schemas.companies import RequestBody, ResponseBody
+from cruds import companies as companies_cruds
+
 logger = getLogger("uvicorn.app")
 
 DbDependency = Annotated[Session, Depends(get_db)]
@@ -13,7 +16,29 @@ DbDependency = Annotated[Session, Depends(get_db)]
 router = APIRouter(prefix="/companies", tags=["Companies"])
 
 
-# 開発時に消してください
-@router.get("", status_code=status.HTTP_200_OK)
-async def sample_func(db: DbDependency):
-	return {}
+@router.post("", response_model=ResponseBody, status_code=status.HTTP_200_OK)
+async def create_companies(db: DbDependency, param: RequestBody):
+
+    try:
+        new_company = companies_cruds.create(db, param)
+        
+        db.commit()
+        
+        re_di = {
+            "company_id": new_company.id,
+            "name": new_company.name,
+            "prefecture": new_company.prefecture,
+            "city": new_company.city,
+            "town": new_company.town,
+            "address": new_company.address,
+            "postal_code": new_company.postal_code,
+            "phone_number": new_company.phone_number,
+            "email": new_company.email
+        }
+
+        return re_di
+    
+    except Exception as e:
+        logger.error(e)
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Invalid input data.")
