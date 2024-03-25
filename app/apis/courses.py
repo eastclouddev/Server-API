054397d4588dda7288ca7a20ea1f309afef7f93b
Inoue-Sub <shoki.inoue.1225.sub@gmail.com>
2024-03-25 16:43:29 +0900

    TAS-177（コース詳細取得API）：実装完了

--- app/apis/courses.py
+++ app/apis/courses.py
@@ -6,7 +6,7 @@ from fastapi import APIRouter, Depends, HTTPException, Path, Query
 from sqlalchemy.orm import Session
 from starlette import status
 
-from schemas.courses import ResponseBody
+from schemas.courses import AllResponseBody, DetailResponseBody
 from cruds import courses as courses_crud
 
 logger = getLogger("uvicorn.app")
@@ -16,7 +16,7 @@ DbDependency = Annotated[Session, Depends(get_db)]
 router = APIRouter(prefix="/courses", tags=["Courses"])
 
 
-@router.get("", response_model=ResponseBody, status_code=status.HTTP_200_OK)
+@router.get("", response_model=AllResponseBody, status_code=status.HTTP_200_OK)
 async def get_courses(db: DbDependency):
     courses = courses_crud.find_by_courses(db)
 
@@ -36,4 +36,45 @@ async def get_courses(db: DbDependency):
         "courses": li
     }
 
+    return re_di
+
+@router.get("/{course_id}", response_model=DetailResponseBody, status_code=status.HTTP_200_OK)
+async def find_course(db: DbDependency, course_id: int = Path(gt=0)):
+    course = courses_crud.find_by_course(db, course_id)
+    sections = courses_crud.find_by_sections(db, course_id)
+
+    if not course:
+        raise HTTPException(status_code=404, detail="Course not found.")
+
+    section_li = []
+    if sections:
+        for section in sections:
+            curriculums = courses_crud.find_by_curriculums(db, section.id)
+            curriculum_li = []
+            # 最内のリストを作成
+            for curriculum in curriculums:
+                curriculum_di = {
+                    "curriculum_id": curriculum.id,
+                    "title": curriculum.title,
+                    "description": curriculum.description
+                }
+                curriculum_li.append(curriculum_di)
+
+            section_di = {
+                "section_id": section.id,
+                "title": section.title,
+                "description": section.description,
+                "curriculums": curriculum_li
+            }
+            section_li.append(section_di)
+    
+    re_di = {
+        "course_id": course.id,
+        "title": course.title,
+        "description": course.description,
+        "created_user_id": course.created_user,
+        "created_at": course.created_at.isoformat(),
+        "sections": section_li
+    }
+	
     return re_di
--- app/schemas/courses.py
+++ app/schemas/courses.py
@@ -17,5 +17,24 @@ class Course(BaseModel):
     thumbnail_url: str
     created_at: str
 
-class ResponseBody(BaseModel):
+class AllResponseBody(BaseModel):
     courses: list[Course]
+
+class CurriculumList(BaseModel):
+    curriculum_id: int
+    title: str
+    description: str
+
+class SectionList(BaseModel):
+    section_id: int
+    title: str
+    description: str
+    curriculums: list[CurriculumList]
+
+class DetailResponseBody(BaseModel):
+    course_id: int
+    title: str
+    description: str
+    created_user_id: int
+    created_at: str
+    sections: list[SectionList]
