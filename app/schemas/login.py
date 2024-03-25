63e302a5158c5408576fac993ae767ef1b8202c2
kotaro-miura <mcotaro070835@gmail.com>
2024-03-25 17:45:13 +0900

    TAS-11(ログインAPI)：実装完了

--- app/schemas/login.py
+++ app/schemas/login.py
@@ -5,9 +5,18 @@ from typing import Optional
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class RequestBody(BaseModel):
-    pass
+class DeviceInfo(BaseModel):
+    device_type: Optional[str] = Field(None, examples=["smartphone"])
+    device_name: Optional[str] = Field(None, examples=["iPhone 12"])
+    uuid: Optional[str] = Field(None, examples=["generated_uuid"])
 
+class RequestBody(BaseModel):
+    email: Optional[str] = Field(None, examples=["user@example.com"])
+    password: Optional[str] = Field(None, examples=["password123"])
+    device_info: DeviceInfo
 
 class ResponseBody(BaseModel):
-    pass
+    user_id: int
+    access_token: str
+    expires_in: int
+    role: str
