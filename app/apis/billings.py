23546c35d9a8c9d8197f77524f1d45423570fa76
kotaro-miura <mcotaro070835@gmail.com>
2024-03-25 17:28:04 +0900

    TAS-208(請求履歴詳細取得API)：実装完了

--- app/apis/billings.py
+++ app/apis/billings.py
@@ -6,14 +6,46 @@ from fastapi import APIRouter, Depends, HTTPException, Path, Query
 from sqlalchemy.orm import Session
 from starlette import status
 
+from cruds import billings as billings
+from schemas.billings import ResponseBody
+
 logger = getLogger("uvicorn.app")
 
 DbDependency = Annotated[Session, Depends(get_db)]
 
 router = APIRouter(prefix="/billings", tags=["Billings"])
 
+@router.get("/{billing_id}",response_model=ResponseBody,status_code=status.HTTP_200_OK)
+async def get_details(db: DbDependency, billing_id: int ):
+    """
+    請求履歴詳細取得
 
-# 開発時に消してください
-@router.get("", status_code=status.HTTP_200_OK)
-async def sample_func(db: DbDependency):
-	return {}
+    Parameter
+    -----------------------
+    billing_id: int
+
+
+    Return
+    ----------------------
+    billing_id: int
+        請求履歴のID
+    company_id: int
+        会社のID
+    date: str
+        請求日（YYYY-MM-DD形式）
+    amount: float
+        請求金額
+    status: string
+        請求状況（例: "paid", "unpaid", "overdue"）
+    payment_details: 
+        支払いの詳細（オプション、支払いが完了している場合に含まれる）
+            payment_method: str
+                支払い方法
+            payment_date: str
+                支払い日（YYYY-MM-DD形式）
+
+    """
+    info = billings.find_by_detail(db, billing_id)
+    if not info:
+        raise HTTPException(status_code=404, detail="Billing record not found")
+    return info
