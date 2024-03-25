c2691eff59228ec29ee6dd0611a7565719482462
Inoue-Sub <shoki.inoue.1225.sub@gmail.com>
2024-03-25 16:05:00 +0900

    TAS-77（アカウント更新API）：実装完了

--- app/apis/users.py
+++ app/apis/users.py
@@ -6,6 +6,9 @@ from fastapi import APIRouter, Depends, HTTPException, Path, Query
 from sqlalchemy.orm import Session
 from starlette import status
 
+from schemas.users import UpdateRequestBody
+from cruds import users as account_update_cruds
+
 logger = getLogger("uvicorn.app")
 
 DbDependency = Annotated[Session, Depends(get_db)]
@@ -13,7 +16,23 @@ DbDependency = Annotated[Session, Depends(get_db)]
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
-# 開発時に消してください
-@router.get("", status_code=status.HTTP_200_OK)
-async def sample_func(db: DbDependency):
-	return {}
+@router.patch("/{user_id}", status_code=status.HTTP_200_OK)
+async def update_account(db: DbDependency, param: UpdateRequestBody, user_id: int = Path(gt=0)):
+	# メールアドレスの重複チェック
+	duplication_user = account_update_cruds.find_by_email(db, param.email, user_id)
+	if duplication_user:
+		raise HTTPException(status_code=400, detail="Email is already in use.")
+
+	try:
+		# 該当のユーザーを更新
+		user = account_update_cruds.update_by_user(db, param, user_id)
+		if not user:
+			raise Exception("NotUser")
+
+		db.commit()
+		return
+
+	except Exception as e:
+		db.rollback()
+		logger.error(e)
+		raise HTTPException(status_code=401, detail="Authentication failed.")
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -5,8 +5,12 @@ from typing import Optional
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class RequestBody(BaseModel):
-    pass
+class UpdateRequestBody(BaseModel):
+    first_name: Optional[str] = Field(None, examples=["花子"])
+    last_name: Optional[str] = Field(None, examples=["山田"])
+    first_name_kana: Optional[str] = Field(None, examples=["ハナコ"])
+    last_name_kana: Optional[str] = Field(None, examples=["ヤマダ"])
+    email: Optional[str] = Field(None, examples=["hanako@example.com"])
 
 
 class ResponseBody(BaseModel):
