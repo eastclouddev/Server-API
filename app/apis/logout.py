148fb491b6cac328a7836f06529fe2e88b58fdae
shibatanaoto0428 <shibatanaoto0428@gmail.com>
2024-03-26 16:23:03 +0900

    TAS-13(ログアウト):実装完了

--- app/apis/logout.py
+++ app/apis/logout.py
@@ -2,7 +2,7 @@ from logging import getLogger
 from typing import Annotated
 
 from database.database import get_db
-from fastapi import APIRouter, Depends, HTTPException, Path, Query
+from fastapi import APIRouter, Depends, HTTPException, Path, Query, Request
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -14,5 +14,12 @@ router = APIRouter(prefix="/logout", tags=["Logout"])
 
 
 @router.post("", status_code=status.HTTP_200_OK)
-async def logout(db: DbDependency):
-	return {}
+async def logout(request: Request):
+
+    req_headers = dict(request.headers)
+    auth = req_headers.get("authorization")
+
+    if not auth:
+        raise HTTPException(status_code=401,detail="Logout failed.")
+
+    return
--- app/schemas/logout.py
+++ app/schemas/logout.py
@@ -11,3 +11,23 @@ class RequestBody(BaseModel):
 
 class ResponseBody(BaseModel):
     pass
+
+class ProductCreate(BaseModel):
+    name: str = Field(min_length=2, max_length=20, examples=["NAME"])
+    price: int = Field(gt=0, examples=[9999])
+    description: Optional[str] = Field(None, examples=["description"])
+
+class ProductUpdate(BaseModel):
+    name: Optional[str] = Field(None, min_length=2, max_length=20, examples=["NAME"])
+    price: Optional[int] = Field(None, gt=0, examples=[9999])
+    description: Optional[str] = Field(None, examples=["description"])
+
+class ProductResponse(BaseModel):
+    id: int = Field(gt=0, examples=[1])
+    name: str = Field(min_length=2, max_length=20, examples=["NAME"])
+    price: int = Field(gt=0, examples=[9999])
+    description: Optional[str] = Field(None, examples=["description"])
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
