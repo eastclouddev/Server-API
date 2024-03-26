8398dfb5cd76f5df4fd1fefc9b13dc2715b89e84
kotaro-miura <mcotaro070835@gmail.com>
2024-03-26 10:57:26 +0900

    TAS-104（カリキュラム詳細取得API）：実装完了

--- app/apis/curriculums.py
+++ app/apis/curriculums.py
@@ -6,6 +6,9 @@ from fastapi import APIRouter, Depends, HTTPException, Path, Query
 from sqlalchemy.orm import Session
 from starlette import status
 
+from cruds import curriculums as curriculums_crud
+from schemas.curriculums import ResponseBody
+
 logger = getLogger("uvicorn.app")
 
 DbDependency = Annotated[Session, Depends(get_db)]
@@ -13,7 +16,37 @@ DbDependency = Annotated[Session, Depends(get_db)]
 router = APIRouter(prefix="/curriculums", tags=["Curriculums"])
 
 
-# 開発時に消してください
-@router.get("", status_code=status.HTTP_200_OK)
-async def sample_func(db: DbDependency):
-	return {}
+@router.get("/{curriculum_id}",response_model=ResponseBody,status_code=status.HTTP_200_OK)
+# @router.get("/billings/{billing_id}",response_model= ResponseBilling)
+
+async def get_details(db: DbDependency, curriculum_id: int ):
+    """
+    カリキュラム詳細取得
+
+    Parameter
+    -----------------------
+    curriculum_id: int
+        詳細を取得したいカリキュラムのID
+
+    Return
+    ----------------------
+    curriculum_id: int
+        カリキュラムのID
+    title: str
+        カリキュラムのタイトル
+    description: str
+        カリキュラムの詳細な説明
+    video_url: str
+        ビデオコンテンツのURL(ビデオが存在する場合のみ）
+    content: str 
+        カリキュラムのテキストコンテンツ(テキストコンテンツが存在する場合のみ）
+    is_test: boolean
+        このカリキュラムがテストかどうかを示すフラグ（boolean）
+    display_no: int
+        カリキュラムの表示順
+
+    """
+    info = curriculums_crud.find_by_detail(db, curriculum_id)
+    if not info:
+        raise HTTPException(status_code=404, detail="Curriculum not found.")
+    return info
--- app/cruds/curriculums.py
+++ app/cruds/curriculums.py
@@ -1,1 +1,32 @@
 from sqlalchemy.orm import Session
+from models.curriculums import Curriculums
+
+
+def find_by_detail(db: Session, curriculum_id: int):
+    curriculum_info = db.query(Curriculums).filter(Curriculums.id == curriculum_id).first()
+    if not curriculum_info:
+        return None
+
+    info = {
+
+    "curriculum_id": curriculum_id,
+    "title": curriculum_info.title,
+    "description": curriculum_info.description,
+    "is_test": curriculum_info.is_test,
+    "display_no": curriculum_info.display_no
+    }
+
+    if curriculum_info.video_url :
+        find_video_url={
+            "video_url": curriculum_info.video_url
+        }
+        info.update(find_video_url)
+        
+    if curriculum_info.content :
+        find_content={
+            "content": curriculum_info.content
+        }
+        info.update(find_content)
+
+
+    return info
