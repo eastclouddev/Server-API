d3ed1b6fedeaf6fcbaf579c38cbe71fb11de47c4
kotaro-miura <mcotaro070835@gmail.com>
2024-03-26 11:06:58 +0900

    TAS-223(自分の質問一覧取得API)：実装完了

--- app/schemas/students.py
+++ app/schemas/students.py
@@ -5,9 +5,19 @@ from typing import Optional
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class RequestBody(BaseModel):
-    pass
+class RequestList(BaseModel):
+    user_id: int 
 
+class ResponseList(BaseModel):
+    id: int
+    title: str
+    content: str
+    curriculum_id: int
+    created_at: datetime
+    is_read: bool
+    is_closed: bool
+      
+    
 
 class ResponseBody(BaseModel):
-    pass
+    questions: list[ResponseList]
