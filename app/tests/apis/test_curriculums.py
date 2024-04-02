65c24cd6b54554348b51469bd5ce3f91ba52d3f3
shibatanaoto0428 <shibatanaoto0428@gmail.com>
2024-04-02 18:43:43 +0900

    TAS-431:(テスト作成）

--- app/tests/apis/test_curriculums.py
+++ app/tests/apis/test_curriculums.py
@@ -2,9 +2,17 @@ from fastapi.testclient import TestClient
 
 """レビュー投稿作成"""
 """成功パターン"""
-def test_questions_01(client_fixture: TestClient):
+def test_reviews_01(client_fixture: TestClient):
 
-    response = client_fixture.post("/curriculums/1/reviews")
+    response = client_fixture.post(
+         "/curriculums/1/reviews",
+         json={
+                 "user_id": 1,
+                 "title": "string",
+                "content": "string",
+                "is_closed": False
+            }
+        )
 
     assert response.status_code == 201
     assert "user_id" in response.json()
@@ -12,19 +20,17 @@ def test_questions_01(client_fixture: TestClient):
     assert "content" in response.json()
     assert "is_closed" in response.json()
 
-# """失敗パターン"""
-# def test_questions_02(client_fixture: TestClient):
+"""失敗パターン"""
+def test_rus_02(client_fixture: TestClient):
 
-#     response = client_fixture.post(
-#        "/curriculums/123/questions",
-#         json={
-#             "user_id": 1,
-#             "title": "string",
-#             "content": "string",
-#             "media_content": {
-#                 "url": "string"
-#             }
-#         } 
-#     )
+    response = client_fixture.post(
+        "/curriculums/123/reviews",
+        json={
+            "user_id": 1,
+            "title": "string",
+            "content": "string",
+            "is_closed": False
+            } 
+    )
 
-#     assert response.status_code == 404 
+    assert response.status_code == 404 
