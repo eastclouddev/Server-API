e9fcb06369e67b710a3baadf03a9b7c33aab0dc7
Inoue-Sub <shoki.inoue.1225.sub@gmail.com>
2024-04-03 15:38:18 +0900

    TAS-420：実装完了

--- app/apis/questions.py
+++ app/apis/questions.py
@@ -6,7 +6,8 @@ from fastapi import APIRouter, Depends, HTTPException, Path, Query
 from sqlalchemy.orm import Session
 from starlette import status
 
-from schemas.questions import CreateRequestBody, CreateResponseBody, DetailResponseBody
+from schemas.questions import CreateRequestBody, CreateResponseBody, DetailResponseBody, \
+                                UpdateAnswerRequestBody, UpdateAnswerResponseBody
 from cruds import questions as questions_crud
 from services import questions as questions_service
 
@@ -155,3 +156,65 @@ async def find_question_thread_details(db: DbDependency, question_id: int):
     }
     
     return re_di
+
+@router.patch("/answers/{answer_id}", response_model=UpdateAnswerResponseBody, status_code=status.HTTP_200_OK)
+async def update_answer(db: DbDependency, param: UpdateAnswerRequestBody, answer_id: int):
+    """
+    質問回答更新（受講生、メンター）
+
+    Parameter
+    -----------------------
+    answer_id: int
+        更新したい回答のID
+    content: str
+        更新したい回答の内容
+    media_content: json
+        更新したい回答に関連するメディアコンテンツの情報
+    is_read: bool
+        回答か既読かを表すフラグ
+
+    Returns
+    -----------------------
+    dict
+        id: int
+            更新された回答のID
+        question_id: int
+            回答が紐づく質問のID
+        user_id: int
+            回答を投稿したユーザーのID
+        parent_answer_id: Optional[int]
+            返信先の回答ID
+        content: str
+            更新された回答の内容
+        media_content: Optional[json]
+            更新されたメディアコンテンツの情報
+        is_read: bool
+            回答が既読かどうかを示すフラグ（boolean）
+        updated_at: str
+            回答が最後に更新された日時（ISO 8601形式）
+    """
+
+    answer = questions_crud.update_answer(db, param, answer_id)
+    if not answer:
+        raise HTTPException(status_code=404, detail="Answer not found.")
+    
+    try:
+        db.commit()
+
+        re_di = {
+            "id": answer.id,
+            "question_id": answer.question_id,
+            "user_id": answer.user_id,
+            "parent_answer_id": answer.parent_answer_id,
+            "content": answer.content,
+            "media_content": answer.media_content,
+            "is_read": answer.is_read,
+            "updated_at": answer.updated_at.isoformat()
+        }
+
+        return re_di
+
+    except Exception as e:
+        logger.error(str(e))
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Invalid input data.")
