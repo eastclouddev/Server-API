d0220bc4072da425938fee4010e2cb6a2cc70c81
kotaro-miura <mcotaro070835@gmail.com>
2024-04-09 18:18:28 +0900

    TAS-526：テスト作成

--- app/tests/apis/test_mentors.py
+++ app/tests/apis/test_mentors.py
@@ -3,3 +3,18 @@ from fastapi.testclient import TestClient
 
 def test_xxx_01(client_fixture: TestClient):
     pass
+
+"""取得成功パターン"""
+def test_progresses_01(client_fixture: TestClient):
+    
+    response = client_fixture.get(
+        "/mentors/1/progresses"
+    )
+    
+    assert response.status_code == 200
+    assert "progress_id" in response.json()["progresses"][0]
+    assert "user_id" in response.json()["progresses"][0]
+    assert "course_id" in response.json()["progresses"][0]
+    assert "section_id" in response.json()["progresses"][0]
+    assert "curriculum_id" in response.json()["progresses"][0]
+    assert "progress_percentage" in response.json()["progresses"][0]
