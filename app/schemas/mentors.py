f73b586eec94d5096db8f6bad003a7ea6076ccae
kotaro-miura <mcotaro070835@gmail.com>
2024-04-24 11:42:50 +0900

    TAS-226(受講生からのレビュー一覧取得API)：実装ファイル間違え　追加

--- app/schemas/mentors.py
+++ app/schemas/mentors.py
@@ -64,3 +64,15 @@ class Question(BaseModel):
 
 class ResponseBody(BaseModel):
     questions: list[Question]
+
+class AllResponseList(BaseModel):
+    id: int 
+    title: str
+    content: str
+    curriculum_id: int
+    created_at: str
+    is_read: bool
+    is_closed: bool
+
+class AllResponseBody(BaseModel):
+    reviews: list[AllResponseList]
--- app/tests/apis/test_mentors.py
+++ app/tests/apis/test_mentors.py
@@ -105,3 +105,19 @@ def test_find_questions(client_fixture: TestClient):
     )
 
     assert response.status_code == 200
+
+
+def test_get_reviews_01(client_fixture: TestClient):
+    
+    response = client_fixture.get(
+        "/mentors/6/students/reviews"
+    )
+    
+    assert response.status_code == 200
+    assert "id" in response.json()["reviews"][0]
+    assert "title" in response.json()["reviews"][0]
+    assert "content" in response.json()["reviews"][0]
+    assert "curriculum_id" in response.json()["reviews"][0]
+    assert "created_at" in response.json()["reviews"][0]
+    assert "is_read" in response.json()["reviews"][0]
+    assert "is_closed" in response.json()["reviews"][0]
