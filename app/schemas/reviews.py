93b07f36a06fdd2771a5bf59eee4fc1c54776903
kotaro-miura <mcotaro070835@gmail.com>
2024-04-24 11:42:23 +0900

    TAS-226(受講生からのレビュー一覧取得API)：実装ファイル間違え　削除

--- app/schemas/reviews.py
+++ app/schemas/reviews.py
@@ -30,18 +30,6 @@ class UpdateReviewResponseBody(BaseModel):
     is_closed: bool
     updated_at: str
 
-class AllResponseList(BaseModel):
-    id: int 
-    title: str
-    content: str
-    curriculum_id: int
-    created_at: str
-    is_read: bool
-    is_closed: bool
-
-class AllResponseBody(BaseModel):
-    reviews: list[AllResponseList]
-
 class ReviewRequestBody(BaseModel):
     id: int
     curriculum_id: int
--- app/tests/apis/test_reviews.py
+++ app/tests/apis/test_reviews.py
@@ -1,21 +1,5 @@
 from fastapi.testclient import TestClient
 
-# ファイルが違う
-def test_reviews_01(client_fixture: TestClient):
-    
-    response = client_fixture.get(
-        "/mentors/2/students/reviews"
-    )
-    
-    assert response.status_code == 200
-    assert "id" in response.json()["reviews"][0]
-    assert "title" in response.json()["reviews"][0]
-    assert "content" in response.json()["reviews"][0]
-    assert "curriculum_id" in response.json()["reviews"][0]
-    assert "created_at" in response.json()["reviews"][0]
-    assert "is_read" in response.json()["reviews"][0]
-    assert "is_closed" in response.json()["reviews"][0]
-
 
 """レビュー回答更新"""
 def test_update_review_response_01(client_fixture: TestClient):
