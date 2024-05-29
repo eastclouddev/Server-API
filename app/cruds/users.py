31ffe1433a5959e2a5ac19856e2c2152943aff99
Inoue-Sub <shoki.inoue.1225.sub@gmail.com>
2024-05-29 15:32:45 +0900

    API修正（users.py）（残タスクあり）

--- app/cruds/users.py
+++ app/cruds/users.py
@@ -1,10 +1,24 @@
 from sqlalchemy.orm import Session
 
-from schemas.users import UserUpdateRequestBody
+from schemas.users import UserCreateRequestBody, UserUpdateRequestBody
 from models.users import Users
 from models.companies import Companies
 from models.roles import Roles
 
+
+def create_user(db: Session, param: UserCreateRequestBody):
+    new_user = Users(
+        company_id = 1,
+        first_name = param.first_name,
+        last_name = param.last_name,
+        first_name_kana = param.first_name_kana,
+        last_name_kana = param.last_name_kana,
+        password = "adbeljldk",
+        email = param.email,
+        role_id = 1,
+    )
+    db.add(new_user)
+    return new_user
 
 def find_user_by_user_id(db: Session, user_id: int):
     return db.query(Users).filter(Users.id == user_id).first()
@@ -12,16 +26,13 @@ def find_user_by_user_id(db: Session, user_id: int):
 def find_role_by_role_id(db: Session, role_id: int):
     return db.query(Roles).filter(Roles.id == role_id).first()
 
-def find_user_by_email(db: Session, email: str, user_id: int):
-    user = db.query(Users).filter(Users.email == email).first()
+def find_company_by_company_id(db: Session, company_id: int):
+    return db.query(Companies).filter(Companies.id == company_id).first()
 
-    # 重複しているユーザーがいない or 更新対象と同じメールアドレス
-    if not user or (user.id == user_id):
-        return None
-    else:
-        return user
+def find_user_by_email(db: Session, email: str):
+    return db.query(Users).filter(Users.email == email).first()
 
-def update_user(db: Session, param:UserUpdateRequestBody, user_id: str):
+def update_user(db: Session, param: UserUpdateRequestBody, user_id: str):
     # 更新対象のユーザーを取得
     user = find_user_by_user_id(db, user_id)
     if not user:
@@ -33,6 +44,7 @@ def update_user(db: Session, param:UserUpdateRequestBody, user_id: str):
     user.first_name_kana = param.first_name_kana
     user.last_name_kana = param.last_name_kana
     user.email = param.email
+    user.is_enable = param.is_enable
     db.add(user)
     return user
 
