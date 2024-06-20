7644709f4770de06adfcbbf74b9e76089a75d44e
shibatanaoto0428 <shibatanaoto0428@gmail.com>
2024-06-20 18:40:17 +0900

    P R-826修正完了

--- app/cruds/billing_summary.py
+++ app/cruds/billing_summary.py
@@ -4,20 +4,18 @@ from datetime import datetime, timedelta
 from models.company_receipts import CompanyReceipts
 
 def find_billing_summary(db: Session):
-    #現在の先月を調べる
     today = datetime.today()
     month = 12 if today.month == 1 else today.month - 1
     year = today.year - 1 if today.month == 1 else today.year
     last_month_str = str(year) + "-" + str(month).zfill(2)
     
-    #現在の先月に紐ずくamountを呼び出す
     company_receipts = db.query(CompanyReceipts)\
                         .filter(CompanyReceipts.payment_date.contains(last_month_str)).all()
 
     total_billed_amount = 0
     for company_receipt in company_receipts:
         total_billed_amount += company_receipt.amount
-        
+
     di = {
         "month": last_month_str,
         "total_billed_amount": total_billed_amount
